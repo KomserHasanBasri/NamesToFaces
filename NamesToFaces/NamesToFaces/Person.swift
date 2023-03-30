70a4d51f2b7ba5a40af121531cd272990d6fa4f4
BasriKomser <101523343+BasriKomser@users.noreply.github.com>
2023-03-30 15:29:53 +0300

    uipickercontroller added

---
+++ NamesToFaces/NamesToFaces/Person.swift
@@ -0,0 +1,12 @@
+//
+//  Person.swift
+//  NamesToFaces
+//
+//  Created by Hasan Basri Komser on 30.03.2023.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+}
---
+++ NamesToFaces/NamesToFaces/PersonCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCollectionViewCell.swift
+//  NamesToFaces
+//
+//  Created by Hasan Basri Komser on 30.03.2023.
+//
+
+import UIKit
+
+class PersonCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var nameLabel: UILabel!
+    
+}
