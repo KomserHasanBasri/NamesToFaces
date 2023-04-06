a08a2813283ed8e499c385929ee7f3cb18a23b43
BasriKomser <101523343+BasriKomser@users.noreply.github.com>
2023-04-06 12:47:18 +0300

    added userDefaults

--- NamesToFaces/NamesToFaces/ViewController.swift
+++ NamesToFaces/NamesToFaces/ViewController.swift
@@ -15,6 +15,18 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(cameraButtonTapped))
         
+        let defaults = UserDefaults.standard
+        if let savedPeople = defaults.object(forKey: "people") as? Data {
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                people = try jsonDecoder.decode([Person].self, from: savedPeople)
+                print(people.count)
+            } catch {
+                print("Failed to load people")
+            }
+        }
+        
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -62,9 +74,11 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         
         let person = Person(name: "Unknown", image: imageName)
         people.append(person)
+        DispatchQueue.main.async { [weak self] in
+            self?.save()
+        }
         collectionView.reloadData()
         dismiss(animated: true)
-        
     }
     
     func getDocumentsDirectory() -> URL {
@@ -73,20 +87,38 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     }
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let person = people[indexPath.item]
-        
+
         let ac = UIAlertController(title: "Change Information", message: nil, preferredStyle: .alert)
         ac.addTextField()
         ac.addAction(UIAlertAction(title: "Rename Photo", style: .default) { [weak self, weak ac] _ in
             guard let newName = ac?.textFields?[0].text else {return}
             person.name = newName
+            DispatchQueue.main.async {
+                self?.save()
+            }
             self?.collectionView.reloadData()
         })
         ac.addAction(UIAlertAction(title: "Delete Photo", style: .destructive){ [weak self] _ in
             self?.people.remove(at: indexPath.item)
+            DispatchQueue.main.async {
+                self?.save()
+            }
             self?.collectionView.reloadData()
+
         })
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         present(ac,animated: true)
+    }
+    
+    func save() {
+        let jsonEncoder = JSONEncoder()
+        if let savedData = try? jsonEncoder.encode(people) {
+            print(people.count)
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "people")
+        } else {
+            print("Failed to save people.")
+        }
     }
 }
 
